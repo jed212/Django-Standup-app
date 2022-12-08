7e5cdfa6517747f442eeaae22a3e87d1d4070d7b
jed212 <jediddahfavour@gmail.com>
2022-12-08 12:01:04 +0300

    Creating REST API

--- standup/views.py
+++ standup/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import commentsSerializer, standupSerializer
 from .models import standup, comments
 
+class standupViewset(viewsets.ModelViewSet):
+    queryset= standup.objects.all()
+    serializer_class = standupSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-@csrf_exempt
-def Updates(request):
-    latest_standup_list = standup.objects.order_by('-pub_date')[:5]
-    comments_list = comments.objects.order_by('-pub_date')[:5]
-    context = ({'latest_standup_list': latest_standup_list},{'comments_list': comments_list})
-    return render(request, 'standup/index.html', context)
 
+class commentsViewset(viewsets.ModelViewSet):
+    queryset = comments.objects.all()
+    serializer_class = commentsSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
